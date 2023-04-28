18dccf89a1d6c1882d51eac2777ac191a618a010
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2023-04-27 23:03:37 -0300

    Add exercises about booleans values

---
+++ js-challenger/basics/booleans/ex1.js
@@ -0,0 +1,8 @@
+// Here, we declare the variable isTrue. But, it has no value yet. Assign a boolean value to it and run the code.
+
+// let isTrue;
+// console.log(isTrue);
+
+let isTrue
+isTrue = true
+console.log(isTrue)
---
+++ js-challenger/basics/booleans/ex2.js
@@ -0,0 +1,11 @@
+// Here, we declare the variable num and assign it the value 5. We also declare the variable bool which we assign the boolean representation of num.
+// Extend the code such that the console.log() statement logs false.
+
+// let num = 5;
+// const bool = Boolean(bool);
+// console.log(bool);
+
+let num = 5
+num = 0
+const bool = Boolean(bool)
+console.log(bool)

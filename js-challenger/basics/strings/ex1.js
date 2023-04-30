73bb6b87219acee9ff6e23f4f41ce93dea25ec3a
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2023-04-29 23:47:57 -0300

    Add exercise about strings

---
+++ js-challenger/basics/strings/ex1.js
@@ -0,0 +1,12 @@
+// After we have learnt how to create JavaScript strings, we will now connect 2 strings together. In the code below we use the Addition (+) operator to concatenate textOne and textTwo. The console.log() statement logs the resulting string. Currently, the result would be HelloWorld.
+// Change the code below so that the value of res is Hello World
+
+// const textOne = 'Hello'
+// const textTwo = 'World'
+// const combined = textOne + textTwo
+// console.log(combined)
+
+const textOne = 'Hello'
+const textTwo = 'World'
+const combined = textOne + ' ' + textTwo
+console.log(combined)

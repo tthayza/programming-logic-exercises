6259e78476a9852261d52fbb079052da07c2fded
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2023-04-29 23:51:49 -0300

    Add more exercises about strings

---
+++ js-challenger/basics/strings/ex2.js
@@ -0,0 +1,13 @@
+// Here, we have declared 3 variables textOne, textTwo, and textThree. An empty string is assigned to all of them.
+// Do you see how in each case different symbols are used to create the string? All three of them are valid methods to create a JavaScript string.
+// Fill in all 3 strings with some characters and run the code to see if the values get logged.
+
+// const textOne = ''
+// const textTwo = ''
+// const textThree = ``
+// console.log(textOne, textTwo, textThree)
+
+const textOne = 'oi '
+const textTwo = 'hello '
+const textThree = `hi`
+console.log(textOne, textTwo, textThree)
---
+++ js-challenger/basics/strings/ex3.js
@@ -0,0 +1,5 @@
+// In this exercise the existing console.log() statement logs the value of the variable text. The variable text has already been declared with an empty string – as indicated by the two single quotes.
+// Fill in the string with some characters and run the code to see if the string is being logged.
+
+const text = 'content'
+console.log('The value of text is: ' + text)

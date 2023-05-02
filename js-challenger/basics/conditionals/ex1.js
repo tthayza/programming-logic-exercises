f72a51e110ff3ed653c245429331e8999a73636a
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2023-05-01 22:35:17 -0300

    Add exercises about conditionals

---
+++ js-challenger/basics/conditionals/ex1.js
@@ -0,0 +1,14 @@
+// The code will assign the variable num a new value 1. But it will only run if the condition is met.
+// Adjust the condition such that the code inside the curly braces will execute and the console.log() statement logs true.
+
+// let num = 0;
+// if (1 > 2) {
+//    num = 1;
+// }
+// console.log(num === 1);
+
+let num = 0
+if (1 < 2) {
+  num = 1
+}
+console.log(num === 1)
---
+++ js-challenger/basics/conditionals/ex2.js
@@ -0,0 +1,14 @@
+// Time to practice what we've learnt so far. In the code below, the if...statement will assign a new value to the variable text. But only if its condition is met. Currently, the condition is missing.
+// Add any condition that will be satisfied such that the console.log() statement logs true.
+
+// let text = 'hello';
+// if () {
+//    text = text + ' world';
+// }
+// console.log(text === 'hello world');
+
+let text = 'hello'
+if (text) {
+  text = text + ' world'
+}
+console.log(text === 'hello world')

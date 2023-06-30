5c2f04de811f285a720699187653a7057a121b63
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2023-06-30 12:34:26 -0300

    Fix descending order exercise

--- python-brasil/decision-structure/ex6.js
+++ python-brasil/decision-structure/ex6.js
@@ -1,12 +1,40 @@
 // Faça um Programa que leia três números e mostre-os em ordem decrescente.
-
-const numbers = [1, 2, 0]
+const first = -99
+const second = -100
+const third = -9
+let biggest
+let smallest
+let middle
 let decreasingNumbers = []
-let smallest = numbers[0]
 
-for (number of numbers) {
-  if (number < smallest) decreasingNumbers.push(number)
-  else decreasingNumbers.unshift(number)
+if (first > second && first > third) {
+  biggest = first
+  if (second > third) {
+    middle = second
+    smallest = third
+  } else {
+    middle = third
+    smallest = second
+  }
+} else if (second > third && second > first) {
+  biggest = second
+  if (third > first) {
+    middle = third
+    smallest = first
+  } else {
+    smallest = third
+    middle = third
+  }
+} else if (third > second && third > first) {
+  biggest = third
+  if (second > first) {
+    middle = second
+    smallest = first
+  } else {
+    middle = first
+    smallest = second
+  }
 }
 
+decreasingNumbers = [smallest, middle, biggest]
 console.log(decreasingNumbers)
